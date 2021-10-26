fd5d406629b8aeca5ef5f1a1143f65886d28a3f1
yugoaja <yugofambudi15@gmail.com>
2021-10-26 16:43:31 +0000

    salasatun

---
+++ biodata.py
@@ -0,0 +1,13 @@
+user = str(input('Enter name : '))
+userid = str(input('Enter nickname  : '))
+npm =int(input('Enter your NPM : '))
+age =int(input('Enter your age : '))
+birthplace =str(input('Place of birth  : '))
+address = str(input('Your address : '))
+nophone = int(input('Enter phone number : '))
+
+print('Assalamualaikum,')
+print('Let me introduce My self. My name is', user,'but you can call me',userid+'.')
+print('My NPM is',npm,'I was born in',birthplace,'and i am',age,'years old.')
+print('I am very  glad if you want to invite my house in',address,'.So, dont forget to call me before with the number',nophone,'.') 
+print('Thank you.')
